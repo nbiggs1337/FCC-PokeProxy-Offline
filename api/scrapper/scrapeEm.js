1561c790d05b83d5b9addcbe82e4979c6b37f103
Noah <81598791+nbiggs1337@users.noreply.github.com>
2024-07-03 12:07:57 -0700

    scrapper init server created

--- api/scrapper/scrapeEm.js
+++ api/scrapper/scrapeEm.js
@@ -4,24 +4,47 @@ const fs = require('fs');
 
 const app = express();
 const port = 3000;
-const url = 'https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/1018/';
+const url = 'https://pokeapi-proxy.freecodecamp.rocks/api/pokemon';
 
 app.get('/scrape', async (req, res) => {
     try {
         // Fetch data from the URL
         const response = await axios.get(url);
-        const data = response.data;
+        const data = response.data.results;
+        let localIndex = [];
 
-        // Write the JSON data to a file
-        fs.writeFile('pokemon.json', JSON.stringify(data, null, 2), (err) => {
-            if (err) {
-                console.error('Error writing file:', err);
-                res.status(500).send('Error writing file');
-            } else {
-                console.log('JSON data saved to pokemon.json');
-                res.send('JSON data saved to pokemon.json');
-            }
-        });
+        data.forEach(async item => {
+            console.log(item.url)
+            //create index in localIndex
+            localIndex.push({ id: item.id, name: item.name, url: `http://localhost:3000/pokemon/${item.id}`})
+            // fetch that pokemon 
+            let fetch = await axios.get(item.url);
+            let resp = fetch.data;
+
+            // save to local file 
+            fs.writeFile(`data/${item.id}.json`, JSON.stringify(resp, null, 2), (err) => {
+                if (err) {
+                    console.error('Error writing file:', err);
+                    res.status(500).send('Error writing file');
+                } else {
+                    console.log(`JSON data saved to ${item.id}.json`);
+                    res.send('JSON data saved to pokemon.json');
+                }
+            })
+        })
+
+        fs.writeFileSync("directory.json", JSON.stringify(localIndex, null, 2))
+
+        // // Write the JSON data to a file
+        // fs.writeFile('pokemon.json', JSON.stringify(data, null, 2), (err) => {
+        //     if (err) {
+        //         console.error('Error writing file:', err);
+        //         res.status(500).send('Error writing file');
+        //     } else {
+        //         console.log('JSON data saved to pokemon.json');
+        //         res.send('JSON data saved to pokemon.json');
+        //     }
+        // });
     } catch (error) {
         console.error('Error fetching data:', error);
         res.status(500).send('Error fetching data');
@@ -29,5 +52,5 @@ app.get('/scrape', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/scrape`);
+    console.log(`Server running- visit http://localhost:${port}/scrape to init data`);
 });
